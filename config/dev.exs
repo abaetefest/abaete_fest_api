f9218ef45eb3b25a6bf491f8319d7703e44f2299
Iago Cavalcante <iagocavalcante@hey.com>
2024-02-13 16:02:47 -0300

    fea: rodando usando docker

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :abaete_fest_api, AbaeteFestApi.Repo,
   username: "postgres",
   password: "postgres",
   database: "abaete_fest_api_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -18,7 +18,7 @@ config :abaete_fest_api, AbaeteFestApi.Repo,
 config :abaete_fest_api, AbaeteFestApiWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
